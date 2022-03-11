5bf1a24c06cf0ab44c00b656bafee38ed9a518ec
Charlo-tech <charliewachira124@gmail.com>
2022-03-10 17:49:41 -0800

    created c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - beginning point
+ *
+ * Return 0 after printing the function
+ *
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	Return(0);
+
+
+
+}
